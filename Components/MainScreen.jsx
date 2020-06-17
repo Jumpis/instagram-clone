73b6bd79a403231cb27363c3e825f8bfd31b48ac
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-06-17 16:31:36 +0900

    changed bottom tab to slide tab

--- Components/MainScreen.jsx
+++ Components/MainScreen.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Platform } from 'react-native';
 import { Icon } from 'native-base';
 import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 import HomeTab from './AppTabNavigator/HomeTab';
 import SearchTab from './AppTabNavigator/SearchTab';
 import AddMediaTab from './AppTabNavigator/AddMediaTab';
 import LikesTab from './AppTabNavigator/LikesTab';
 import ProfileTab from './AppTabNavigator/ProfileTab';
 
-const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab },
-  SearchTab: { screen: SearchTab },
-  AddMediaTab: { screen: AddMediaTab },
-  LikesTab: { screen: LikesTab },
-  ProfileTab: { screen: ProfileTab },
-});
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    HomeTab: { screen: HomeTab },
+    SearchTab: { screen: SearchTab },
+    AddMediaTab: { screen: AddMediaTab },
+    LikesTab: { screen: LikesTab },
+    ProfileTab: { screen: ProfileTab },
+  },
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      style: {
+        ...Platform.select({
+          ios: {
+            backgroundColor: 'white',
+          },
+          android: {
+            backgroundColor: 'white',
+          },
+        }),
+      },
+      iconStyle: { height: 30 },
+      activeTintColor: '#000',
+      inactiveTintColor: '#d1cece',
+      upperCaseLabel: false,
+      showLabel: false,
+      showIcon: true,
+    },
+  }
+);
 
 const AppTabContainer = createAppContainer(AppTabNavigator);
 
@@ -31,7 +56,7 @@ const MainScreen = () => {
   return (
     // <View style={styles.container}>
     //   <Text>MainScreen</Text>
-      <AppTabContainer />
+    <AppTabContainer />
     // </View>
   );
 };
